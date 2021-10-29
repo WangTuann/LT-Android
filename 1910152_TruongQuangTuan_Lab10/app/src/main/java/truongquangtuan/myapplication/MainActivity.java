90f46151a22526c16b1a4024ab63ddb9cd0d75a9
Your Name <you@example.com>
2021-10-29 10:49:27 +0700

    hoan thanh app Truyen Cuoi

---
+++ 1910152_TruongQuangTuan_Lab10/app/src/main/java/truongquangtuan/myapplication/MainActivity.java
@@ -0,0 +1,41 @@
+package truongquangtuan.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class MainActivity extends AppCompatActivity {
+    private String topicName;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        showFrg(new M000SplashFrg());
+    }
+    private void showFrg(Fragment frg){
+        getSupportFragmentManager().beginTransaction().replace(R.id.ln_main,frg,null).commit();
+    }
+    public void gotoM001Screen() {
+        getSupportFragmentManager().beginTransaction().replace(R.id.ln_main, new
+                M001TopicFrg(), null).commit();
+    }
+    public void gotoM002Screen(String topicName) {
+    this.topicName=topicName;
+    M002StoryFrg frg=new M002StoryFrg();
+    frg.setTopicName(topicName);
+    showFrg(frg);
+    }
+    public void backToM001Screen() {
+
+        gotoM001Screen();
+    }
+    public void gotoM003Screen(ArrayList<StoryEntity> listStory, StoryEntity story){
+        M003StoryFrg frg = new M003StoryFrg();
+        frg.setData(topicName, listStory, story);
+        showFrg(frg);
+    }
+}
